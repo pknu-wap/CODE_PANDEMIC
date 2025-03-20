35ff81bc36116898dfc3212e2929d394314f916a
hallow0312 <leejaehyun0312@naver.com>
2025-03-21 01:43:37 +0900

    Feat:UIBase

---
+++ CODE_PANDEMIC/Assets/Scripts/Scene/BaseScene.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Define;
+public class BaseScene : MonoBehaviour
+{
+    public SceneType _sceneType = SceneType.Unknown;
+    protected bool _init;
+    private void Awake()
+    {
+        Init();
+    }
+    protected virtual bool Init()
+    {
+        if (_init) return false;
+        _init = true;
+
+        Managers.Init();
+        GameObject obj = GameObject.Find("EventSystem");
+        if (obj == null)
+        {
+           Managers.Resource.Instantiate("EventSystem", null);
+
+       }
+        return true;
+    }
+    public void FadeIn() //scene 넘어갈때 fade in fade out
+    {
+        //ui  manager ?
+
+    }
+
+    public void FadeOut()
+    {
+
+    }
+}
