c29c1a21039c30373e9a49d3003f6869e7a0d269
wave1009 <wave.061009@gmail.com>
2025-03-27 17:37:42 +0900

    Style: class변경

--- CODE_PANDEMIC/Assets/Scripts/Monster/AI_Controller.cs
+++ CODE_PANDEMIC/Assets/Scripts/Monster/AI_Controller.cs
@@ -2,18 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AI_Zombie : MonoBehaviour
+private enum AI_State
+{
+    Idle,
+    Walk,
+    Attack,
+    Dead
+}
+
+public class AI_Controller : MonoBehaviour
 {
     private Transform _player;
-    private Rigidbody2D _aiRb;
+    private Rigidbody2D _Rb;
     private Coroutine _aiDamageCoroutine;
 
-    private SpriteRenderer _aiRenderer;
+    private SpriteRenderer _Renderer;
 
     private float _aiSpeed = 2f;
     private float _aiDamage = 10f;
     private float _aiDetectionRange = 7.5f; // 시야 거리
-    private float _aiDetectionAngle = 120f; // 시야각각
+    private float _aiDetectionAngle = 120f; // 시야각
     private float _aiDamageDelay = 0.5f; // 데미지 딜레이(닿였을때 대미지 들어오는 간격)
     public virtual bool Init()
     {
@@ -25,16 +33,16 @@ public class AI_Zombie : MonoBehaviour
         }
         _player = playerObj.transform;
 
-        _aiRb = GetComponent<Rigidbody2D>();
-        if (_aiRb == null)
+        _Rb = GetComponent<Rigidbody2D>();
+        if (_Rb == null)
         {
             Debug.LogError("Rigidbody 없음");
             return false;
         }
-        _aiRb.freezeRotation = true;
+        _Rb.freezeRotation = true;
 
-        _aiRenderer = GetComponent<SpriteRenderer>();
-        if (_aiRenderer == null)
+        _Renderer = GetComponent<SpriteRenderer>();
+        if (_Renderer == null)
         {
             Debug.LogError("Sprite 없음");
             return false;
@@ -50,14 +58,17 @@ public class AI_Zombie : MonoBehaviour
         }
     }
 
-
+    private void Update(){
+        float currentAngle = AI_CalculateAngle();
+        float currentDistance = AI_CalculateDistance();
+    }
     private void FixedUpdate()
     {
         if (_player == null)
             return;
 
-        float currentAngle = AI_Angle();
-        float currentDistance = AI_Distance();
+        float currentAngle = AI_CalculateAngle();
+        float currentDistance = AI_CalculateDistance();
 
         if (currentAngle <= _aiDetectionAngle / 2) // 시야각 계산 
         {
@@ -67,16 +78,16 @@ public class AI_Zombie : MonoBehaviour
             }
             else
             {
-                AI_Stop();
+                AI_StopMoving();
             }
         }
         else
         {
-            AI_Stop();
+            AI_StopMoving();
         }
     }
 
-    private float AI_Angle()
+    private float AI_CalculateAngle()
     {
         Vector2 aiDirection = (_player.position - transform.position).normalized;
         Vector2 aiForward = (_player.position.x < transform.position.x) ? Vector2.left : Vector2.right;
@@ -84,19 +95,19 @@ public class AI_Zombie : MonoBehaviour
         return Vector2.Angle(aiForward, aiDirection);
     }
 
-    private float AI_Distance()
+    private float AI_CalculateDistance()
     {
         return Vector2.Distance(_player.position, transform.position);
     }
 
     private void ChasePlayer(Vector3 target)
     {
-        Vector2 newPosition = Vector2.MoveTowards(_aiRb.position, target, _aiSpeed * Time.fixedDeltaTime);
-        _aiRb.MovePosition(newPosition);
-        _aiRenderer.flipX = _player.position.x < transform.position.x; // 플레이어가 왼쪽에 있을 때 왼쪽 보게함 
+        Vector2 newPosition = Vector2.MoveTowards(_Rb.position, target, _aiSpeed * Time.fixedDeltaTime);
+        _Rb.MovePosition(newPosition);
+        _Renderer.flipX = _player.position.x < transform.position.x; // 플레이어가 왼쪽에 있을 때 왼쪽 보게함 
     }
 
-    private void AI_Stop()
+    private void AI_StopMoving()
     {
         // 추후 Idle 애니메이션 추가 예정
     }
@@ -124,12 +135,9 @@ public class AI_Zombie : MonoBehaviour
 
     private IEnumerator AI_Damage()
     {
-        while (true)
-        {
             Debug.Log(_aiDamage + " 데미지");
             // 플레이어 체력 감소 로직 추가 예정 
             yield return new WaitForSecondsRealtime(_aiDamageDelay);
-        }
     }
 
 }
