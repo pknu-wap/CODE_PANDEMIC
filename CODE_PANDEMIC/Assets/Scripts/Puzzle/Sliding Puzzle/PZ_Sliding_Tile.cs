2a55fba3b009db08e2a62fb8a7a64a97252523f0
wave1009 <wave.061009@gmail.com>
2025-03-27 17:27:54 +0900

    Squashed commit of the following:
    commit ee7efdc280b84479f07461e390750bba774d56c0
    Merge: aefbddf3 81b233ee
    Author: 이재현 <153084573+hallow0312@users.noreply.github.com>
    Date:   Thu Mar 27 15:35:30 2025 +0900
    
        Merge pull request #8 from pknu-wap/Feature/Puzzle
    
        Feature/puzzle 비밀 번호 퍼즐, 피아노 퍼즐, 슬라이딩 퍼즐(그림) 구현
    
    commit 81b233eed99917907e422ec1802781f1f273cefa
    Author: KaneBigNose <imddongs@naver.com>
    Date:   Thu Mar 27 00:22:35 2025 +0900
    
        Fix: 생성 시기 수정
    
    commit e37665ad5cbcb25a656224dc440c668eb1526aa6
    Author: KaneBigNose <imddongs@naver.com>
    Date:   Sun Mar 23 16:57:13 2025 +0900
    
        Feat: Piano Puzzle 구현
    
    commit f435d732c55e81a9bbab36ebc4f9c742c3941ca0
    Author: KaneBigNose <imddongs@naver.com>
    Date:   Sun Mar 23 15:33:40 2025 +0900
    
        Feat: 비밀 번호 입력 퍼즐 구현
    
        But 입력된 비밀 번호 출력 오류가 있음 ㅠㅠ
    
    commit 7f645fbbe95616787b36e009197e5dfda7b59b9a
    Author: KaneBigNose <imddongs@naver.com>
    Date:   Fri Mar 21 23:03:37 2025 +0900
    
        Feat: Sliding puzzle tile sprites 추가
    
        tile의 이미지를 테스트 이미지에서 추가된 sprite의 이미지로 변경 및 로직 일부 수정
    
    commit bc9a1d931b237b46592a5a47a50cef903e8b290b
    Author: KaneBigNose <imddongs@naver.com>
    Date:   Thu Mar 20 23:57:15 2025 +0900
    
        Feat: Notion Background Image 추가
    
    commit adf42f0e2a2b15f244fd01f65cb2b7901ea866a3
    Merge: 0c41fe72 aefbddf3
    Author: KaneBigNose <imddongs@naver.com>
    Date:   Thu Mar 20 23:43:50 2025 +0900
    
        Merge branch 'Develop' into Feature/Puzzle
    
    commit 0c41fe721d70de93b8a68330309f241ec25795fc
    Author: KaneBigNose <imddongs@naver.com>
    Date:   Thu Mar 20 23:43:39 2025 +0900
    
        Feat: 테스트용 그림 퍼즐 추가
    
        그림은 추후 게임의 힌트와 관련된 그림으로 변경 예정

---
+++ CODE_PANDEMIC/Assets/Scripts/Puzzle/Sliding Puzzle/PZ_Sliding_Tile.cs
@@ -0,0 +1,117 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections;
+using System.Collections.Generic; // 삭제 예정
+
+public class PZ_Sliding_Tile : MonoBehaviour, IPointerClickHandler
+{
+    private TextMeshProUGUI _tileNumberText; // 테스트용 현재 타일 번호 출력을 위한 text
+    private int _tileNumber; // 현재 타일 번호
+
+    private PZ_Sliding_Board _slidingBoard;
+    private Vector3 _correctPosition;
+    public bool _isCorrect = false;
+
+    private Image _image; // 퍼즐 이미지
+
+    // 해당 부분은 임시로 이렇게 함 나중에 resource 폴더에서 리소스 로드로 구현 할 예정
+    [SerializeField]
+    private List<Sprite> _puzzleSprites;
+
+    // 삭제 예정
+    public int TileNumber
+    {
+        get { return _tileNumber; }
+
+        set
+        {
+            _tileNumber = value;
+            _tileNumberText.text = _tileNumber.ToString();
+        }
+    }
+
+    public bool Init()
+    {
+        _slidingBoard = GetComponentInParent<PZ_Sliding_Board>();
+        _image = GetComponent<Image>();
+
+        if (!_slidingBoard || !_image)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    // tileEmptyIndex 마지막 빈 타일의 index
+    public void TileSetup(int tileNumber, int tileEmptyIndex)
+    {
+        _tileNumberText = GetComponentInChildren<TextMeshProUGUI>();
+        TileNumber = tileNumber;
+        _image.sprite = _puzzleSprites[tileNumber - 1];
+
+        if (TileNumber == tileEmptyIndex)
+        {
+            GetComponent<Image>().enabled = false;
+            _tileNumberText.enabled = false;
+        }
+    }
+
+    // 정답 위치 설정
+    public void SetCorrectPosition()
+    {
+        _correctPosition = GetComponent<RectTransform>().localPosition;
+    }
+
+    // 클릭 이벤트
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Click" + _tileNumber);
+
+        _slidingBoard.MoveTile(this);
+    }
+
+    public void TileMoveto(Vector3 endPosition)
+    {
+        StartCoroutine("TileMove", endPosition);
+    }
+
+    // 타일 자연스럽게 움직이기
+    private IEnumerator TileMove(Vector3 endPosition)
+    {
+        float currentTime = 0f;
+        float currentPecentage = 0f;
+        float moveDuration = 0.1f;
+        Vector3 startPosition = GetComponent<RectTransform>().localPosition;
+
+        while (currentPecentage < 1)
+        {
+            currentTime += Time.deltaTime;
+            currentPecentage = currentTime / moveDuration;
+            GetComponent<RectTransform>().localPosition = Vector3.Lerp(startPosition, endPosition, currentPecentage);
+
+            yield return null;
+        }
+
+        // 타일 위치 체크
+        _isCorrect = CheckCorrectPosition();
+
+        // 퍼즐 클리어 체크
+        _slidingBoard.IsPuzzleClear();
+    }
+
+    // 현재 타일 위치가 올바른 위치인지 확인
+    private bool CheckCorrectPosition()
+    {
+        if (GetComponent<RectTransform>().localPosition == _correctPosition)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
