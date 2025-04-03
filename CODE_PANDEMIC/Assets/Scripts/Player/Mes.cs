b36f2b4732c7af18b9d0b9eee5c6cece9691142f
doranayoung <kny4345@gmail.com>
2025-04-04 00:07:48 +0900

    feat:: branch초기화

---
+++ CODE_PANDEMIC/Assets/Scripts/Player/Mes.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using UnityEngine;
+
+
+public class Mes : MonoBehaviour
+{
+    private float speed = 12f;
+    private float range = 10f;
+
+    private Vector2 direction;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            direction = (mousePos - (Vector2)transform.position).normalized;
+            StartCoroutine(ThrowMace());
+        }
+    }
+
+    IEnumerator ThrowMace()
+    {
+        float distanceTravelled = 0f;
+        while (distanceTravelled < range)
+        {
+            transform.Translate(direction * speed * Time.deltaTime);
+            distanceTravelled += speed * Time.deltaTime;
+            yield return null;
+        }
+        Destroy(gameObject);
+    }
+}
