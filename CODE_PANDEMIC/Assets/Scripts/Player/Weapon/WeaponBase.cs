b6d47bb780500877a53af9ad9dd296591e71c6df
doranayoung <kny4345@gmail.com>
2025-04-16 18:57:33 +0900

    feat:: reload override 수정

--- CODE_PANDEMIC/Assets/Scripts/Player/Weapon/WeaponBase.cs
+++ CODE_PANDEMIC/Assets/Scripts/Player/Weapon/WeaponBase.cs
@@ -40,7 +40,4 @@ public abstract class WeaponBase : MonoBehaviour
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0, 0, angle - 90f);
     }
-
-        }
-
-
+}
