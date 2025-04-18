cce60087cdd2e3404bd37953dfde497b96028072
KaneBigNose <imddongs@naver.com>
2025-04-18 18:42:00 +0900

    Feat: 힐팩 아이템 추가 및 배치

---
+++ CODE_PANDEMIC/Assets/Scripts/Puzzle/Interact/HealPack/PZ_HealPack.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PZ_HealPack : MonoBehaviour, IInteractable
+{
+    private bool _isInteracted = false;
+    private SpriteRenderer _spriteRenderer;
+    [SerializeField] private Sprite _interactedSprite;
+
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // 벽 힐팩 상호 작용
+    public void Interact()
+    {
+        if (_isInteracted)
+        {
+            return;
+        }
+
+        _spriteRenderer.sprite = _interactedSprite;
+
+        Debug.Log("회복 아이템 획득");
+
+        // 여기에 아이템 획득 기능 구현
+
+        _isInteracted = true;
+    }
+}
