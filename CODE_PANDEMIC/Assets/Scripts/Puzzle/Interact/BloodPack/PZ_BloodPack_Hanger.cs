0cd6497e2b78ad3510c794733ee4f4f912efcf7b
KaneBigNose <imddongs@naver.com>
2025-04-28 20:22:44 +0900

    Feat: 혈액 팩 추가

---
+++ CODE_PANDEMIC/Assets/Scripts/Puzzle/Interact/BloodPack/PZ_BloodPack_Hanger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PZ_BloodPack_Hanger : PZ_Interact_Base
+{
+    [SerializeField] private Sprite _interactedSprite;
+
+    public override void Interact(GameObject player)
+    {
+        if (_isInteracted)
+        {
+            return;
+        }
+
+        base.Interact(player);
+
+        _spriteRenderer.sprite = _interactedSprite;
+
+        Managers.Resource.Instantiate("PZ_BloodPack_Prefab", null, (getObject) =>
+        {
+            getObject.GetComponent<PZ_BloodPack>().Setting(player.GetComponent<PlayerController>());
+        });
+    }
+}
