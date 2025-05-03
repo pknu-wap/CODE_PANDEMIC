0e0820742f9083c7a98741e55c3b65b32237a043
KaneBigNose <imddongs@naver.com>
2025-05-03 20:31:23 +0900

    Feat: 자동차 퍼즐 기본 기능 구현
    자동차 기본적인 이동 구현 (상호 작용 방식)

---
+++ CODE_PANDEMIC/Assets/Scripts/Puzzle/Interact/Car/PZ_Car.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum CarMoveDirection
+{
+    None,
+    North,
+    West,
+    South,
+    East
+}
+
+public class PZ_Car : PZ_Interact_NonSpawn
+{
+    [SerializeField] private Rigidbody2D _rigidbody;
+
+    [SerializeField] private bool _isMainCar = false;
+    [SerializeField] private bool _isVerticalCar = false;
+
+    public int[] _body1Index = new int[2];
+    public int[] _body2Index = new int[2];
+
+    private CarMoveDirection _direction = CarMoveDirection.None;
+
+    private float _moveValue = 3f;
+
+    public override void Interact(GameObject player)
+    {
+        if (_isInteracted)
+        {
+            return;
+        }
+
+        base.Interact(player);
+
+        StartCoroutine(MoveCar());
+    }
+
+    private IEnumerator CheckDirection()
+    {
+        yield return new WaitUntil(() =>
+        Input.GetKeyDown(KeyCode.W) ||
+        Input.GetKeyDown(KeyCode.A) ||
+        Input.GetKeyDown(KeyCode.S) ||
+        Input.GetKeyDown(KeyCode.D)
+        );
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            _direction = CarMoveDirection.North;
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            _direction = CarMoveDirection.West;
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            _direction = CarMoveDirection.South;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            _direction = CarMoveDirection.East;
+        }
+    }
+
+    private IEnumerator MoveCar()
+    {
+        yield return StartCoroutine(CheckDirection());
+
+        Vector2 currentPos = _rigidbody.position;
+        Vector2 destinationPos = _rigidbody.position;
+
+        if (_direction == CarMoveDirection.North)
+        {
+            destinationPos.y += _moveValue;
+        }
+        else if (_direction == CarMoveDirection.West)
+        {
+            destinationPos.x -= _moveValue;
+        }
+        else if (_direction == CarMoveDirection.South)
+        {
+            destinationPos.y -= _moveValue;
+        }
+        else if (_direction == CarMoveDirection.East)
+        {
+            destinationPos.x += _moveValue;
+        }
+
+        float currentTime = 0f;
+        float currentPercent = 0f;
+        float moveDuration = 0.5f;
+
+        while (currentTime <= 1)
+        {
+            currentTime += Time.fixedDeltaTime;
+            currentPercent = currentTime / moveDuration;
+
+            _rigidbody.position = Vector2.Lerp(currentPos, destinationPos, currentPercent);
+
+            yield return new WaitForFixedUpdate();
+        }
+
+        _isInteracted = false;
+        _direction = CarMoveDirection.None;
+    }
+}
---
+++ CODE_PANDEMIC/Assets/Scripts/Puzzle/Interact/Car/PZ_Parking.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PZ_Parking : MonoBehaviour
+{
+    private bool[,] _emptyPlace = new bool[6, 6];
+
+    private void Start()
+    {
+        _emptyPlace[2, 0] = true;
+
+        _emptyPlace[4, 1] = true;
+
+        _emptyPlace[3, 2] = true;
+        _emptyPlace[4, 2] = true;
+        _emptyPlace[5, 2] = true;
+
+        _emptyPlace[1, 4] = true;
+
+        _emptyPlace[3, 5] = true;
+        _emptyPlace[5, 5] = true;
+    }
+}
