39adc42244b4719ea17d4c2de15d2681f2ad0b70
KaneBigNose <imddongs@naver.com>
2025-05-11 19:42:39 +0900

    Feat: 공사장 문, 컨테이너 추가

---
+++ CODE_PANDEMIC/Assets/Scripts/Puzzle/Road/Interact/Door/PZ_DoorTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System;
+
+public class PZ_DoorTrigger : MonoBehaviour
+{
+    public static event Action CloseWorksiteDoor;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        CloseWorksiteDoor?.Invoke();
+        Destroy(gameObject);
+    }
+}
---
+++ CODE_PANDEMIC/Assets/Scripts/Puzzle/Road/Trigger/Container/PZ_Container.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PZ_Container : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        _animator.SetBool("IsOpened", true);
+
+        // 여기에 좀비를 소환하는 로직 추가 예정
+        // 컨테이너에서 쏟아져 나오는 듯한 연출 넣을 예정
+
+        Destroy(gameObject);
+    }
+}
