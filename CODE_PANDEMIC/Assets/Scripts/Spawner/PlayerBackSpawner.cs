ebdddcf1e8a25f846543c1e0100e8ee347ebdb7a
hallow0312 <leejaehyun0312@naver.com>
2025-05-13 17:22:35 +0900

    Feat: 이전맵 스포너

--- CODE_PANDEMIC/Assets/Scripts/Spawner/PlayerBackSpawner.cs
+++ CODE_PANDEMIC/Assets/Scripts/Spawner/PlayerBackSpawner.cs
@@ -3,21 +3,19 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class PlayerSpawner : SpawnBase
-{ 
+public class PlayerBackSpawner : SpawnBase
+{
     public override void SpawnObjects()
     {
         Managers.Object.RegisterSpawners();
-        if (Managers.Game.IsNextStage() == false) return;
+        if (Managers.Game.IsNextStage()) return;
         Managers.Resource.Instantiate("Player", null, (obj) =>
         {
-            obj.transform.position=transform.position;   
-            obj.GetOrAddComponent<PlayerStatus>().SetInfo(); 
+            obj.transform.position = transform.position;
+            obj.GetOrAddComponent<PlayerStatus>().SetInfo();
             Managers.Object.RegisterPlayer(obj.GetComponent<PlayerStatus>());
-           
+
         });
 
     }
-   
 }
-   
