1b897c44332bfb262ae0069b7f137ba443b13d92
KaneBigNose <imddongs@naver.com>
2025-05-14 13:57:37 +0900

    Feat: 컨테이너에서 좀비 소환 추가

--- CODE_PANDEMIC/Assets/Scripts/Puzzle/Road/Trigger/Container/PZ_Container.cs
+++ CODE_PANDEMIC/Assets/Scripts/Puzzle/Road/Trigger/Container/PZ_Container.cs
@@ -1,8 +1,13 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class PZ_Container : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
+
+    [SerializeField] private GameObject _zombiePrefab;
+    [SerializeField] private Transform _spawnTransform;
+    [SerializeField] private int _zombieCount = 3;
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
@@ -13,8 +18,29 @@ public class PZ_Container : MonoBehaviour
 
         _animator.SetBool("IsOpened", true);
 
-        // 여기에 좀비를 소환하는 로직 추가 예정
-        // 컨테이너에서 쏟아져 나오는 듯한 연출 넣을 예정
+        StartCoroutine(SpawnZombies());
+    }
+
+    private IEnumerator SpawnZombies()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        Vector3 spawnPos = _spawnTransform.position;
+        spawnPos.y -= 1f;
+
+        Vector2 forceVec = new Vector2(0, -3);
+
+        while (_zombieCount-- > 0)
+        {
+            GameObject zombie = Instantiate(_zombiePrefab, spawnPos, Quaternion.identity);
+            Rigidbody2D rigidbody2D = zombie.GetComponent<Rigidbody2D>();
+
+            yield return new WaitForSeconds(0.1f);
+
+            rigidbody2D.AddForce(forceVec, ForceMode2D.Impulse);
+
+            yield return new WaitForSeconds(0.5f);
+        }
 
         Destroy(gameObject);
     }
