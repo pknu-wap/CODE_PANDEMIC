74c2aba8067688e0cd892c11142fd8cd2024d293
hallow0312 <leejaehyun0312@naver.com>
2025-05-24 04:07:37 +0900

    Feat :보스 스포너

--- CODE_PANDEMIC/Assets/Scripts/Camera/VirtualCamera/CinematicCamera.cs
+++ CODE_PANDEMIC/Assets/Scripts/Camera/VirtualCamera/CinematicCamera.cs
@@ -1,13 +1,9 @@
-using System;
+
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class CinematicCamera :VirtualCameraBase
 {
-
     protected override bool Init()
     {
         if (base.Init()==false) return false;
@@ -19,23 +15,27 @@ public class CinematicCamera :VirtualCameraBase
         Managers.Event.Subscribe("Cinematic", OnCinematic);
         Managers.Event.Subscribe("EndCinematic", OnEndCinematic);
     }
-
+    private void OnDisable()
+    {
+        Managers.Event.Unsubscribe("Cinematic", OnCinematic);
+        Managers.Event.Unsubscribe("EndCinematic", OnEndCinematic);
+    }
     private void OnEndCinematic(object obj)
     {
         _camera.Priority = Define.None;
-       if(obj is  Define.CinematicType type)
-       {
-            switch(type)
+        if (obj is Define.CinematicType type)
+        {
+            switch (type)
             {
                 case Define.CinematicType.PuzzleClear:
                     StartCoroutine(EndPuzzleCinematic());
                     break;
-                case Define.CinematicType.BossSequence: //자연스럽게 돌아가게 
+                case Define.CinematicType.BossSequence:
+                    _camera.Priority= Define.None; //smooth step
                     break;
             }
-       }
+        }
     }
-    
     IEnumerator EndPuzzleCinematic()
     {
         Managers.UI.FadeAtOnce();
@@ -47,10 +47,5 @@ public class CinematicCamera :VirtualCameraBase
         _camera.Priority = Define.Cinematic;
     }
 
-    private void OnDisable()
-    {
-        Managers.Event.Unsubscribe("Cinematic", OnCinematic);
-        Managers.Event.Unsubscribe("EndCinematic", OnEndCinematic);
-    }
-
+  
 }
