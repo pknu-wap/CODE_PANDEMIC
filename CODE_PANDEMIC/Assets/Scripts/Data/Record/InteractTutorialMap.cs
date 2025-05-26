f758de36febe7c41f8544b622888fd033d2a9d03
jaehyeon <leejaehyun0312@naver.com>
2025-05-26 13:25:04 +0900

    Feat: 튜토리얼 SO

---
+++ CODE_PANDEMIC/Assets/Scripts/Data/Record/InteractTutorialMap.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "Data/InteractTutorialData")]
+public class InteractTutorialMapSO : ScriptableObject
+{
+    public List<InteractTutorialEntry> Entries = new();
+
+    [Serializable]
+    public class InteractTutorialEntry
+    {
+        public InteractType Type;
+        public string PopupPrefabName;
+    }
+
+    public string GetPopupName(InteractType type)
+    {
+        var entry = Entries.Find(e => e.Type == type);
+        return entry?.PopupPrefabName;
+    }
+}
