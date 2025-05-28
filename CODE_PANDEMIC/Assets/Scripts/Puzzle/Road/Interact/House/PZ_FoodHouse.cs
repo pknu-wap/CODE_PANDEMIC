63d1c5ca11933f6552aea486a5d802d32e104ada
KaneBigNose <imddongs@naver.com>
2025-05-28 19:53:16 +0900

    Feat: 음식점 추가
    Fix: 컨테이너 근처에서 대쉬가 사용되지 않던 현상 수정

---
+++ CODE_PANDEMIC/Assets/Scripts/Puzzle/Road/Interact/House/PZ_FoodHouse.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PZ_FoodHouse : PZ_Interact_Spawn
+{
+    public override void Interact(GameObject player)
+    {
+        if (_isInteracted)
+        {
+            return;
+        }
+
+        base.Interact(player);
+
+        RewardItem();
+    }
+}
