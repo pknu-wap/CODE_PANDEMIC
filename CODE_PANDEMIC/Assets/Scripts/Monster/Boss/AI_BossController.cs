757760bc1811d44267affd6da74b208719c7400a
wave1009 <wave.061009@gmail.com>
2025-07-03 21:42:24 +0900

    Feat: 보스 컨트롤러 및 보스 사망 애니메이션

---
+++ CODE_PANDEMIC/Assets/Scripts/Monster/Boss/AI_BossController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using UnityEngine;
+
+public abstract class AI_BossController : AI_Controller
+{
+    [SerializeField] private CinematicCamera _camera;
+    [SerializeField] protected ShockWave _wave;
+    protected bool _isCinematicStarted = false;
+    public bool IsBerserk { get; protected set; }
+    protected int MaxHealth => _monsterData.Hp;
+
+    protected override void Start()
+    {
+        StartCoroutine(BossStartSequence());
+        base.Start();
+    }
+
+    protected IEnumerator BossStartSequence()
+    {
+        if (_camera == null) yield break;
+        _camera.gameObject.SetActive(true);
+        _camera.OnCinematic();
+        Managers.Event.InvokeEvent("OnCinematicStart");
+        //TODO: BOSSSEQUENCE
+        yield return CoroutineHelper.WaitForSeconds(2.0f);
+        _wave.gameObject.SetActive(true);
+        _wave.CallShockWave();
+        yield return CoroutineHelper.WaitForSeconds(1.0f);
+        _camera.OnEndCinematic(Define.CinematicType.BossSequence);
+        Managers.Event.InvokeEvent("OnCinematicEnd");
+    }
+
+
+    public override void TakeDamage(int amount)
+    {
+        if (_isDead) return;
+        base.TakeDamage(amount);
+
+        if (!IsBerserk && Health <= MaxHealth * 0.5f)
+        {
+            EnterBerserkMode();
+        }
+
+        if (Health <= 0f && _currentState is not AI_StateDie)
+        {
+            _isDead = true;
+            Skill?.StopSkill();
+            _rb.velocity = Vector2.zero;
+            StopMoving();
+            Managers.Game.ClearBoss(_monsterData.TemplateID);
+            StartCoroutine(BossDeathSequence());
+            ChangeState(new AI_StateDie(this));
+        }
+    }
+
+    protected virtual void EnterBerserkMode()
+    {
+        IsBerserk = true;
+    }
+
+    protected IEnumerator BossDeathSequence()
+    {
+        Managers.Event.InvokeEvent("OnBossClear");
+        yield return CoroutineHelper.WaitForSeconds(2);
+    }
+}
